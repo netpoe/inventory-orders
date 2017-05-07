5dc49329af36fd9f25a601dcc10e2d70acbe2da7
Gus <yo@soygus.com>
2017-05-07 18:43:11 -0500

    Adds edit link to cart menu

--- resources/views/includes/products_cart/menu.blade.php
+++ resources/views/includes/products_cart/menu.blade.php
@@ -1,7 +1,7 @@
 <?php $currentRoute = \Request::route()->getName(); ?>
 <header class="front-dashboard-menu">
   @if (app('request')->route('order'))
-    <a href="{{ route('cart:index', ['order' => app('request')->route('order')->id]) }}"
+    <a href="{{ route('cart:edit', ['order' => app('request')->route('order')->id]) }}"
       class="menu-item {{ preg_match('/cart:index/', $currentRoute) ? 'active' : '' }}"><span>Carrito</span></a>
     <a href="{{ route('cart:shipping', ['order' => app('request')->route('order')->id]) }}"
       class="menu-item {{ preg_match('/cart:shipping/', $currentRoute) ? 'active' : '' }}"><span>Envío</span></a>
@@ -16,6 +16,6 @@
       class="menu-item {{ preg_match('/cart:shipping/', $currentRoute) ? 'active' : '' }}"><span>Envío</span></a>
     <a href="#"
       class="menu-item {{ preg_match('/cart:checkout/', $currentRoute) ? 'active' : '' }}"><span>Pago</span></a>
-      <div class="menu-item {{ preg_match('/orders:confirmation/', $currentRoute) ? 'active' : '' }}"><span>Confirmación</span></div>
+    <div class="menu-item {{ preg_match('/orders:confirmation/', $currentRoute) ? 'active' : '' }}"><span>Confirmación</span></div>
   @endif
 </header>
